efdabecd347393e919b6debb4e0e5b026051e758
Arjun <arjunketha@gmail.com>
2025-02-10 16:31:44 +0530

    added whole sigin functionality with api connections

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import { Button } from '@/components/ui/button';
-import { Auth } from './pages/auth/Auth';
-import { SignInCard } from '@/components/organisms/Auth/SignInCard';
-import { SignupCard } from '@/components/organisms/Auth/SignupCard';
-import { Notfound } from './pages/Notfound/Notfound';
+import { Auth } from '@/pages/auth/Auth';
+// import { SignupCard } from '@/components/organisms/Auth/SignupCard';
+import { Notfound } from '@/pages/Notfound/Notfound';
+import { QueryClient } from '@tanstack/react-query';
+import { SignupContainer } from '@/components/organisms/Auth/SignupContainer';
+import { Toaster } from '@/components/ui/toaster';
+import { SigninContainer } from './components/organisms/Auth/SignInContainer';
 function App() {
-
+  const queryClient=new QueryClient();
   return (
-    <Routes>
-      <Route path="/auth/signup" element={<Auth><SignupCard/></Auth>}/>
-      <Route path="/auth/signin" element={<Auth><SignInCard/></Auth>}/>
+    <QueryClientProvider client={queryClient} >
+      <Routes>
+      <Route path="/auth/signup" element={<Auth><SignupContainer/></Auth>}/>
+      <Route path="/auth/signin" element={<Auth><SigninContainer/></Auth>}/>
+      <Route path="/home" element={<Auth>helll00000000</Auth>}/>
 
-      <Route path="/*" element={<Notfound/>}/>
+      <Route path="/*" element={<Notfound/>}/>  
     </Routes>
+    <Toaster position="top-center"/>  
+      </QueryClientProvider>
+    
   );
 }
 
--- src/components/organisms/Auth/SignInCard.jsx
+++ src/components/organisms/Auth/SignInCard.jsx
@@ -1,65 +1,94 @@
-import { useState } from 'react';
+import { LucideLoader2, TriangleAlert } from 'lucide-react';
+import { FaCheck } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
-import { useNavigate } from 'react-router-dom';
 
-export const SignInCard = () => {
+export const SigninCard = ({
+    signinForm,
+    setSigninForm,
+    onSigninFormSubmit,
+    validationError,
+    error,
+    isSuccess,
+    isPending
+}) => {
 
-    const navigate=useNavigate();
-    const [signInForm, setSignInForm] = useState({
-        
-        email: '',
-        password: '',
-        
-        
-    });
+    const navigate = useNavigate();
+
+    
 
     return (
         <Card classname="w-full h-full">
             <CardHeader>
                 <CardTitle>Sign In</CardTitle>
-                <CardDescription>Sign In to access your account</CardDescription>
+                <CardDescription>Sign in to access your account</CardDescription>
+
+                {validationError && (
+                    <div className='bg-destructive/15 p-4 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6'>
+                        <TriangleAlert className='size-5' />
+                        <p>{validationError.message}</p>
+                    </div>
+                )}
+
+                {error && (
+                    <div className='bg-destructive/15 p-4 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6'>
+                        <TriangleAlert className='size-5' />
+                        <p>{error.message}</p>
+                    </div>
+                )}
+
+                {isSuccess && (
+                    <div className='bg-primary/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-primary mb-5'>  
+                        <FaCheck className='size-5' />
+                        <p>
+                            Successfully signed in. You will be redirected to the home page in a few seconds.
+                            <LucideLoader2 className="animate-spin ml-2" />
+                        </p>
+                    </div>
+                )}
+
             </CardHeader>
+
             <CardContent>
-                <form className='space-y-3'>
+                <form className='space-y-3' onSubmit={onSigninFormSubmit}>
                     <Input
+                        disabled={isPending}
                         placeholder="Email"
                         required
-                        onChange={(e) => setSignInForm({ ...signInForm, email: e.target.value })}
-                        value={signInForm.email}
                         type="email"
-                        disabled={false}
+                        value={signinForm.email}
+                        onChange={(e) => setSigninForm({ ...signinForm, email: e.target.value })}
                     />
                     <Input
+                        disabled={isPending}
                         placeholder="Password"
                         required
-                        onChange={(e) => setSignInForm({ ...signInForm, password: e.target.value })}
-                        value={signInForm.password}
                         type="password"
-                        disabled={false}
+                        value={signinForm.password}
+                        onChange={(e) => setSigninForm({ ...signinForm, password: e.target.value })}
                     />
-                    <Button 
-                        disabled={false}
-                        size="lg"
-                        type="submit"
-                        className="w-full"
-                    >
+
+                    <Button className="w-full" disabled={isPending} size="lg" type="submit">    
                         Continue
                     </Button>
                 </form>
 
-                <Separator className="my-5" />
+                <Separator className='my-5' />
 
                 <p
                     className='text-s text-muted-foreground mt-4'
                 >
-                    Don't have an account ? {' '}
+                    Donot have an account ? {' '}
                     <span 
-                    className='text-sky-600 hover:underline cursor-pointer'
-                    onClick={()=>navigate('/auth/signup')}>Sign Up</span>
+                        className='text-sky-600 hover:underline cursor-pointer'
+                        onClick={() => navigate('/auth/signup')}
+                    >
+                        Sign Up
+                    </span>
                 </p>
             </CardContent>
         </Card>
