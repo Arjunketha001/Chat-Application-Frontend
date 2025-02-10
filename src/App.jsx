7df05d4975e3d3ed2e67e74124c33188ed40dcea
Arjun <arjunketha@gmail.com>
2025-02-10 10:16:28 +0530

    added signup ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import { Button } from '@/components/ui/button';
+import { Auth } from './pages/auth/Auth';
 function App() {
 
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Routes>
+      <Route path="/auth" element={<Auth/>}></Route>
+    </Routes>
   );
 }
 
--- src/hooks/auth/useSignup.js
+++ src/hooks/auth/useSignup.js
@@ -1,7 +1,11 @@
+
+// mutation is for making changes in server side
+// query is for fetching data from server side
 import { useMutation } from '@tanstack/react-query';
 
 import { signUpRequest } from '@/apis/auth';
 
+// wherever in ui ,u can call signupMutation function to make a request to server
 export const useSignup = () => {
     const { isPending, isSuccess, error, mutate: signupMutation } = useMutation({
         mutationFn: signUpRequest,
