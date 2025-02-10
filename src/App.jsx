627d3380962db2b2de32ca4c849ddaa66c02b518
Arjun <arjunketha@gmail.com>
2025-02-11 00:08:32 +0530

    added   combiner provider and local storage of token

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 
-import { Button } from '@/components/ui/button';
-import { Auth } from '@/pages/auth/Auth';
-// import { SignupCard } from '@/components/organisms/Auth/SignupCard';
-import { Notfound } from '@/pages/Notfound/Notfound';
 import { QueryClient } from '@tanstack/react-query';
-import { SignupContainer } from '@/components/organisms/Auth/SignupContainer';
 import { Toaster } from '@/components/ui/toaster';
-import { SigninContainer } from './components/organisms/Auth/SignInContainer';
+import { AppContextProvider } from './context/AppContextProvider';
+import { AppRoutes } from '@/Routes';
 function App() {
   const queryClient=new QueryClient();
   return (
     <QueryClientProvider client={queryClient} >
-      <Routes>
-      <Route path="/auth/signup" element={<Auth><SignupContainer/></Auth>}/>
-      <Route path="/auth/signin" element={<Auth><SigninContainer/></Auth>}/>
-      <Route path="/home" element={<Auth>helll00000000</Auth>}/>
-
-      <Route path="/*" element={<Notfound/>}/>  
-    </Routes>
-    <Toaster position="top-center"/>  
+      <AppContextProvider>
+        <AppRoutes/>
+      <Toaster position="top-center"/>  
+    </AppContextProvider>
       </QueryClientProvider>
     
   );
---
+++ src/utlis/combineContext.jsx
@@ -0,0 +1,32 @@
+
+//  ... is rest operator
+
+export default function combineContext(...providers) {
+    /**
+     * This combines multiple context provider together and returns a single context provider
+     */
+    return ({ children }) => {
+        return providers.reduceRight((accumulator, Currentprovider) => {
+            return <Currentprovider>{accumulator}</Currentprovider>;
+        }, children /* Initial Value */);
+    };
+}
+
+
+/**
+ * <A>
+ *  <B>
+ *      <C>
+ *          <D>
+ *          {children}
+ *         </D>
+ *      </C>
+ *   </B>
+ * </A>
+ */
+
+/**
+ * <Combined>
+ * {children}
+ * </Combined>
+ */
