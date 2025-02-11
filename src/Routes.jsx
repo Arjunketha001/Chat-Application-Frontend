4a745a258ed8a0b1a12bbc1f2020050f01b7c533
Arjun <arjunketha@gmail.com>
2025-02-11 12:41:42 +0530

    added  logout functionality

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,13 +5,14 @@ import { Auth } from '@/pages/auth/Auth';
 import { SigninContainer } from '@/components/organisms/Auth/SigninContainer';
 import { Notfound } from '@/pages/Notfound/Notfound';
 import { ProtectedRoute } from '@/components/molecules/ProtectedRoute/ProtectedRoute';
+import { Home } from '@/pages/Home/Home';
 
 export const AppRoutes=()=>{
     return (
         <Routes>
         <Route path="/auth/signup" element={<Auth><SignupContainer/></Auth>}/>
         <Route path="/auth/signin" element={<Auth><SigninContainer/></Auth>}/>
-        <Route path="/home" element={<ProtectedRoute><Auth>helll00000000</Auth></ProtectedRoute>}/>
+        <Route path="/home" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
 
         <Route path="/*" element={<Notfound/>}/>  
       </Routes>
