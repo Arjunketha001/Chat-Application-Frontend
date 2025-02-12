0a24694ae9a4321bd272067d99a46a8764442e73
Arjun <arjunketha@gmail.com>
2025-02-12 15:24:18 +0530

    added workspaceswitcher and resizeable sidebar to show channel

--- src/pages/Workspace/Layout.jsx
+++ src/pages/Workspace/Layout.jsx
@@ -1,14 +1,32 @@
-import { WorkspaceSidebar } from "@/components/organisms/Workspace/WorkspaceSidebar";
-
+import { WorkspaceNavbar } from '@/components/organisms/Workspace/WorkspaceNavbar';
+import { WorkspaceSidebar } from '@/components/organisms/Workspace/WorkspaceSidebar';
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 
 export const WorkspaceLayout = ({ children }) => {
     return (
-    <div className="h-[100vh]">
-        <div className="flex h-full">
-            <WorkspaceSidebar />
-            {children}
+        <div className="h-[100vh]">
+            <WorkspaceNavbar />
+            <div className="flex h-[calc(100vh-40px)]">
+                <WorkspaceSidebar />
+                <ResizablePanelGroup direction="horizontal" autoSaveId={'workspace-resize'}>
+                    <ResizablePanel
+                        defaultSize={20}
+                        minSize={11}
+                        className='bg-slack-medium'
+                    >
+                        <div>
+                            Sidebar
+                        </div>
+                    </ResizablePanel>
+                    <ResizableHandle withHandle/>
+                    <ResizablePanel
+                        minSize={20}
+                    >
+                        {children}
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+                
+            </div>
         </div>
-    </div>
-
-)
+    );
 };
